8952cd54451b2a4b5e3cd9ee38b6c829eac217d4
juakim <gmldnjs0971@uos.ac.kr>
2020-05-26 16:59:32 +0900

    update

--- square/4-CNN/model_structure.py
+++ square/4-CNN/model_structure.py
@@ -1,9 +1,13 @@
 import torch
 import torchviz
 from torchviz import make_dot
+import os
+os.environ["CUDA_VISIBLE_DEVICES"]="0"
 
 def save_model_structure(model,modelnumber):
+  device = torch.device('cuda')
   x = torch.rand(1,4,51,51)
+  x = x.to(device)
   y = model(x)
 
   dot = make_dot(y.mean(), params=dict(model.named_parameters()))
--- square/4-CNN/version_model/model_list.py
+++ square/4-CNN/version_model/model_list.py
@@ -153,13 +153,8 @@ class Model4(nn.Module):
             nn.BatchNorm2d(128),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=False),
-
-            nn.Conv2d(128,256,3, padding=(1, 1)),
-            nn.BatchNorm2d(256),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=False),
             
-            nn.Conv2d(256,256,3, padding=(1, 1)),
+            nn.Conv2d(128,256,3, padding=(1, 1)),
             nn.BatchNorm2d(256),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=False),
@@ -168,10 +163,38 @@ class Model4(nn.Module):
             nn.BatchNorm2d(512),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=False),
+            
+            nn.AdaptiveAvgPool2d(output_size=(1, 1))
+            )
 
-            nn.Conv2d(512,512,3, padding=(1, 1)),
+        self.layer2 = nn.Sequential(
+            nn.Dropout(p=0.5, inplace=False),
+            nn.Linear(in_features=512, out_features=1, bias=True)
+        )
+        
+    def forward(self, x):
+        x = self.layer1(x)
+        x = x.mean(dim=(2,3))
+        x = self.layer2(x)
+        return x
+
+class Model5(nn.Module):
+    def __init__(self):
+        super(Model5,self).__init__()
+
+        self.layer1 =  nn.Sequential(
+            nn.Conv2d(4,128,3,bias=False),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True),
+            
+            nn.Conv2d(128,256,3, padding=(1, 1)),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+            
+            nn.Conv2d(256,512,3, padding=(1, 1)),
             nn.BatchNorm2d(512),
             nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2, padding=0, dilation=1, ceil_mode=False),
             
             nn.AdaptiveAvgPool2d(output_size=(1, 1))
             )
