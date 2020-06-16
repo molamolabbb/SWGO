9be0e6a04e368821d1fe5822a3c82461855a32a8
juakim <gmldnjs0971@uos.ac.kr>
2020-06-16 16:45:37 +0900

    update upper pmt only

--- square/4-CNN/save_log.py
+++ square/4-CNN/save_log.py
@@ -1,12 +1,19 @@
 import pandas as pd
 import os
 from datetime import datetime
+import h5py 
 
-def save_log(losses, accuracy,epoch,n): 
-  accloss = pd.DataFrame([losses['train'],losses['valid'],accuracy['train'],accuracy['valid']],
-             columns=list(range(epoch)),index=['loss_train','loss_val','acc_train','acc_val'])
+def save_log(losses, accuracy,epoch,n,upperlower): 
+  data = {'loss_train':losses['train'], 'loss_val':losses['valid'],'acc_train':accuracy['train'],'acc_val':accuracy['valid']}
+  accloss = pd.DataFrame(data)
   now = datetime.now() 
-  acclosslogdir = "save_model/accloss/bin{}/".format(n) + now.strftime("%Y%m%d-%H%M%S") + "/"
+  acclosslogdir = "save_model/{}/accloss/bin{}/".format(upperlower, n) + now.strftime("%Y%m%d-%H%M%S") + "/"
   os.mkdir(acclosslogdir)
   
-  accloss.to_csv(acclosslogdir+"accloss.csv", mode='a')
+  accloss.to_csv(acclosslogdir+"accloss.csv", mode='a', index=False)
+
+def save_efficiency(tpr, fpr, n,upperlower):
+    hf = h5py.File( "save_model/{}/efficiency/efficiency_B{}.h5".format(upperlower,n),'w')
+    hf.create_dataset('tpr', data=tpr)
+    hf.create_dataset('fpr', data=fpr)
+    hf.close()
